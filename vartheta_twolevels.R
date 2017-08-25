ec3fad2d852ab19567eea9a6e46cbf756d51af15
klgrantham <kelsey.grantham@monash.edu>
2017-08-25 10:57:39 +1000

    Create functions to: calculate variance of treatment effect at mean and individual levels, generate covariance matrices at mean and individual levels, generate design matrices

---
+++ vartheta_twolevels.R
@@ -0,0 +1,219 @@
+# Investigating the variance of the treatment effect at the
+# cluster-period mean level and the individual level
+#
+# Kelsey Grantham (kelsey.grantham@monash.edu)
+
+# Equations for variance of treatment effect
+# Mean and individual level
+
+vartheta_mean <- function(Vi, Xmat){
+  # Calculates the variance of the treatment effect, theta, for a model at the
+  # cluster mean level with a particular treatment schedule
+  #
+  # Inputs:
+  # Xmat - a K x T matrix of the treatment schedule (note: all elements either 0 or 1)
+  # Vi - a T x T variance matrix for one cluster
+  
+  K <- nrow(Xmat)
+  Tp <- ncol(Xmat)
+  Xvec <- as.vector(t(Xmat))
+  var <- 1/(t(Xvec) %*% (diag(1,K) %x% solve(Vi)) %*% Xvec - 
+            colSums(Xmat) %*% solve(Vi) %*% (matrix(colSums(Xmat),nrow=Tp, ncol=1))/K)
+  return(var)
+}
+
+vartheta_ind <- function(Vi, Xmat){
+  # Calculates the variance of the treatment effect, theta, for a model at the
+  # individual level with a particular treatment schedule
+  #
+  # Inputs:
+  # Xmat - a K x T matrix of the treatment schedule (note: all elements either 0 or 1)
+  # Vi - a Tm x Tm variance matrix for one cluster
+  
+  K <- nrow(Xmat)
+  Tp <- ncol(Xmat)
+  m <- nrow(Vi)/Tp
+  # If continuous time matrix, use Toeplitz inversion algorithm
+  if(Vi[1,2]!=Vi[1,3]){
+    Vi_inv <- TrenchInverse(Vi)
+  } else{
+    Vi_inv <- solve(Vi)
+  }
+  Q <- Xmat %x% t(rep(1,m))
+  B <- colSums(Xmat) %x% rep(1,m)
+  C <- diag(Tp) %x% rep(1,m)
+  term1 <- sum(diag(Q %*% Vi_inv %*% t(Q))) # Previously: t(D) %*% (diag(1,K) %x% Vi_inv) %*% D, where D <- Xvec %x% rep(1,m)
+  term2 <- t(B) %*% Vi_inv %*% C
+  term3 <- solve(t(C) %*% Vi_inv %*% C)
+  term4 <- t(C) %*% Vi_inv %*% B
+  var <- 1/(term1 - (1/K)*term2 %*% term3 %*% term4)
+  return(var)
+}
+
+splitcomp <- function(rowind, mat1, mat2){
+  mult <- t(mat1[rowind,]) %*% mat2 %*% mat1[rowind,]
+  return(mult)
+}
+
+vartheta_ind_alt <- function(Vi, Xmat){
+  # Calculates the variance of the treatment effect, theta, for a model at the
+  # individual level with a particular treatment schedule
+  #
+  # Inputs:
+  # Xmat - a K x T matrix of the treatment schedule (note: all elements either 0 or 1)
+  # Vi - a Tm x Tm variance matrix for one cluster
+  
+  K <- nrow(Xmat)
+  Tp <- ncol(Xmat)
+  m <- nrow(Vi)/Tp
+  Vi_inv <- TrenchInverse(Vi)
+  Q <- Xmat %x% t(rep(1,m))
+  B <- colSums(Xmat) %x% rep(1,m)
+  C <- diag(Tp) %x% rep(1,m)
+  term1 <- sum(laply(seq(1,nrow(Q)), splitcomp, Q, Vi_inv))
+  term2 <- t(B) %*% Vi_inv %*% C
+  term3 <- solve(t(C) %*% Vi_inv %*% C)
+  term4 <- t(C) %*% Vi_inv %*% B
+  var <- 1/(term1 - (1/K)*term2 %*% term3 %*% term4)
+  return(var)
+}
+
+
+# Variance matrices under different models
+# Hussey & Hughes, discrete time decay, continuous time decay
+
+HHVi <- function(Tp, m, rho0, meanlvl=TRUE){
+  # Constructs the variance matrix for a single cluster, Vi, under the
+  # Hussey & Hughes model (2007), at either the cluster mean level or
+  # at the individual level
+  #
+  # Inputs:
+  # Tp - number of time periods
+  # m - number of individuals per cluster
+  # rho0 - proportion of total variation attributed to cluster random effects
+  # meanlvl - boolean for whether to construct the variance matrix for the
+  #           cluster mean level (default) or the individual level
+
+  totalvar <- 1
+  tau2 <- rho0*totalvar
+  sig2E <- totalvar - tau2
+  sig2 <- sig2E/m # subject-specific variance at mean level
+  if(meanlvl==TRUE){
+    Vi <- diag(sig2,Tp) + tau2*matrix(1, nrow=Tp, ncol=Tp)
+  }
+  else{
+    Vi <- diag(sig2E,Tp*m) + tau2*matrix(1, nrow=Tp*m, ncol=Tp*m)
+  }
+  return(Vi)
+}
+
+expdecayVi <- function(r, Tp, m, rho0, meanlvl=TRUE){
+  # Constructs the variance matrix for a single cluster, Vi, under the
+  # exponential decay model (Kasza et al 2017), at either the cluster-
+  # period mean level or at the individual level
+  #
+  # Inputs:
+  # Tp - number of time periods
+  # m - number of individuals per cluster
+  # rho0 - proportion of total variation attributed to cluster-period random effects
+  # meanlvl - boolean for whether to construct the variance matrix for the
+  #           cluster-period mean level (default) or the individual level
+
+  totalvar <- 1
+  sig2CP <- rho0*totalvar
+  sig2E <- totalvar - sig2CP
+  sig2 <- sig2E/m
+  if(meanlvl==TRUE){
+    Vi <- diag(sig2,Tp) +
+            sig2CP*(r^abs(matrix(1:Tp, nrow=Tp, ncol=Tp, byrow=FALSE) -
+                          matrix(1:Tp, nrow=Tp, ncol=Tp, byrow=TRUE)))    
+  }
+  else{
+    Vi <- diag(sig2E,Tp*m) +
+            sig2CP*(r^abs(matrix(rep(1:Tp, each=m), nrow=Tp*m, ncol=Tp*m, byrow=FALSE) -
+                          matrix(rep(1:Tp, each=m), nrow=Tp*m, ncol=Tp*m, byrow=TRUE)))
+  }
+  return(Vi)
+}
+
+expdecayVicont <- function(r, Tp, m, rho0, meanlvl=FALSE){
+  # Constructs the variance matrix for a single cluster, Vi, under the
+  # exponential decay model in continuous itme, at either the cluster-
+  # period mean level or at the individual level
+  #
+  # Inputs:
+  # Tp - number of time periods
+  # m - number of individuals per cluster
+  # rho0 - proportion of total variation attributed to cluster-period random effects
+  # meanlvl - boolean for whether to construct the variance matrix for the
+  #           cluster-period mean level or the individual level (default)
+  
+  totalvar <- 1
+  sig2CP <- rho0*totalvar
+  sig2E <- totalvar - sig2CP
+  sig2 <- sig2E/m
+  if(meanlvl==TRUE){
+    if(r==1){
+      Vi <- expdecayVi(r=r, Tp=Tp, m=m, rho0=rho0, meanlvl=meanlvl) # Reverts to Hussey and Hughes
+    }
+    else{
+      vars <- (1/m^2)*(m + 2*((r^(1/m))*(-m*(r^(1/m)) + m + r - 1))/((r^(1/m)) - 1)^2) # to be multiplied by sig2CP and added to sig2
+      covars <- (1/m^2)*(((r^((1/m)-1))*(r-1)^2)/((r^(1/m)) - 1)^2) # to be multiplied by sig2CP and r^{|j-j'|}
+      mat <- matrix(covars, nrow=Tp, ncol=Tp) - diag(covars,Tp) + diag(vars,Tp)
+      Vi <- diag(sig2,Tp) +
+            (sig2CP*(r^abs(matrix(1:Tp, nrow=Tp, ncol=Tp, byrow=FALSE) -
+                           matrix(1:Tp, nrow=Tp, ncol=Tp, byrow=TRUE)))*mat)  
+    }
+  }
+  else{
+      Vi <- diag(sig2E,Tp*m) +
+        sig2CP*(r^(abs(matrix(rep(1:(Tp*m)), nrow=Tp*m, ncol=Tp*m, byrow=FALSE) -
+                       matrix(rep(1:(Tp*m)), nrow=Tp*m, ncol=Tp*m, byrow=TRUE))/m))
+  }
+  return(Vi)
+}
+
+# Design matrices (or treatment schedules) for different trial designs
+# Stepped wedge (SW), cluster randomised crossover (CRXO), parallel, parallel w/baseline
+
+SWdesmat <- function(Tp){
+  Xsw <- matrix(data=0, ncol=Tp, nrow=(Tp-1))
+  for(i in 1:(Tp-1)){
+    Xsw[i,(i+1):Tp] <- 1
+  }
+  return(Xsw)
+}
+
+crxodesmat <- function(Tp){
+  if(Tp%%2==0) {
+    nclust <- Tp
+  } else {
+    nclust <- Tp-1
+  }
+  Xcrxo <- matrix(data=0, ncol=Tp, nrow=nclust)
+  Xcrxo[1:nclust/2, seq(1,Tp,2)] <- 1
+  Xcrxo[(nclust/2 + 1):nclust, seq(2,Tp,2)] <- 1
+  return(Xcrxo)
+}
+
+plleldesmat <- function(Tp){
+  if(Tp%%2==0) {
+    nclust <- Tp
+  } else {
+    nclust <- Tp-1
+  }
+  Xpllel <- matrix(data=0, ncol=Tp, nrow=nclust)
+  Xpllel[1:nclust/2,] <- 1
+  return(Xpllel)
+}
+
+pllelbasedesmat <- function(Tp){
+  if(Tp%%2==0) {
+    nclust <- Tp
+  } else {
+    nclust <- Tp-1
+  }
+  Xpllelbase <- matrix(data=0, ncol=Tp, nrow=nclust)
+  Xpllelbase[1:nclust/2, 2:Tp] <- 1
+  return(Xpllelbase)
+}
